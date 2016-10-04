67ffbc319129d9b37654c0f5f8eeab0530659b15
pequaad <noahknam@gmail.com>
2016-10-04 15:50:11 -0400

    first commit

---
+++ Sublet Finder/TermAvailableButton.swift
@@ -0,0 +1,21 @@
+//
+//  TermAvailableButton.swift
+//  Sublet Finder
+//
+//  Created by AppLabAdmin on 9/26/16.
+//  Copyright © 2016 AppLabAdmin. All rights reserved.
+//
+
+import UIKit
+
+class TermAvailableButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
